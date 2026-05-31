4caaee4c87bca08d87ba67f3307bd1174d661585
Wang Maggie <amaokox@gmail.com>
2026-05-31 16:35:39 +0800

    fix: remove sitemap package, use custom sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://BlackCat-MimiW.github.io',
   base: '/augmented-thinking',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
 });
